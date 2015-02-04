1ab682fa511bd6c611ff01a3a04f7e23d2d83e15
Kellyn Flanagan <kflanagan@lat-kflanagan1.local>
2015-02-03 19:23:58 -0500

    adding premium jdp

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'jobs/jdp_premium' => 'jobs#jdp_premium'
   get 'recommendations' => 'recommendations#index'
   get 'resumes' => 'resumes#index'
+  get 'resumes/resume_preview' => 'resumes#resume_preview'
   get 'resumes/cover_letter' => 'resumes#cover_letter'
   get 'resumes/resume_selected' => 'resumes#resume_selected'
   get 'resumes/cover_letter_blank' => 'resumes#cover_letter_blank'
