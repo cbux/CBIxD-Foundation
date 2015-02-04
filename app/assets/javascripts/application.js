588ea1ae5f3e054e7cae8fad33be93e23588854d
James Matchett <jamesc.matchett@gmail.com>
2015-02-03 23:54:11 -0500

    dyanmic password validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,66 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(document)
+  .foundation({
+    abide : {
+			// patterns: {
+			// cbpassword: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,15}$/
+			// }
+			validators: {
+        passwordcb: function(el, required, parent) {
+					var a = el.value;
+					var lower = /[a-z]/.test(a);
+					var upper = /[A-Z]/.test(a);
+					var symnum = /[0-9$&+,:;=?@#|\/\\\"\'<>.^*()%!-]/.test(a);
+					var length = a.length;
+					if(lower === true) {
+						var b = document.getElementById('lowercase-count');
+						b.classList.add('green')
+						b.classList.remove('red');
+					}
+					else {
+						var b = document.getElementById('lowercase-count');
+						b.classList.add('red')
+						b.classList.remove('green');
+					}
+					if(upper === true) {
+						var c = document.getElementById('uppercase-count');
+						c.classList.add('green')
+						c.classList.remove('red');
+					}
+					else {
+						var c = document.getElementById('uppercase-count');
+						c.classList.add('red')
+						c.classList.remove('green');
+					}
+					if(symnum === true) {
+						var d = document.getElementById('number-or-symbol');
+						d.classList.add('green')
+						d.classList.remove('red');
+					}
+					else {
+						var d = document.getElementById('number-or-symbol');
+						d.classList.add('red')
+						d.classList.remove('green');
+					}
+					if(length >= 8 && length <= 15) {
+						var e = document.getElementById('character-count');
+						e.classList.add('green')
+						e.classList.remove('red');
+					}
+					else {
+						var e = document.getElementById('character-count');
+						e.classList.add('red')
+						e.classList.remove('green');
+					}
+        }
+      }
+    }
+  });
 
+
+// Mobile Search Dropdown Visiblity Toggle
 function toggle_visibility(button, id) {
   var e = document.getElementById(id);
   var f = document.getElementById(button);
@@ -32,7 +90,7 @@ function toggle_visibility(button, id) {
     f.style.color = '#888888';
   }
 }
-
+// Password Show Hide Button State Change
 function toggle_password_text(id) {
 	var p = document.getElementById(id);
 	if(p.innerHTML == "Show") {
