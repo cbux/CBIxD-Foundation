69c414dd2f5fabf77817d8c02a9290d821500e6f
James Matchett <jamesc.matchett@gmail.com>
2015-02-04 14:03:35 -0500

    using partials across pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+	before_action :auth
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def auth
+  	if params[:auth] && params[:auth] == "unknown" || params[:auth] == "known" || params[:auth] == "authenticated" ||
+  		session[:auth] = params[:auth]
+  	end
+  	@auth = session[:authentication]
+  	@auth ||= "unknown"
+  end
 end
