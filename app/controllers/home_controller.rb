8a697bb3e692a3f0046dc5ffb425727644b8fdbc
James Matchett <jamesc.matchett@gmail.com>
2015-02-19 11:45:44 -0500

    messaging on delete of cover letter or resume

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	before_action :debug
+	before_action :debug, :statuses
 	def index
 		if session[:auth] == "authenticated"
 			render 'dashboard'
@@ -9,6 +9,14 @@ class HomeController < ApplicationController
 	end
 
 	private
+	def statuses
+		if params[:status] == "resume_deleted"
+			@success_message = "Your resume has been deleted"
+		end
+		if params[:status] == "cover_letter_deleted"
+			@success_message = "Your cover letter has been deleted"
+		end
+	end
   def debug
   	if params[:action] == "index"
   		@statuses = {"sign out" => {:signed_out => "true"}, "account deleted" => {:deleted_account => "true"}}
