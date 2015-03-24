6aa47a452c16500ff33bae2fcabe2cd476b4036e
James Matchett <jamesc.matchett@gmail.com>
2015-03-24 14:36:22 -0400

    update from james

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,8 @@ Rails.application.routes.draw do
   get 'home-three' => 'home#experiment-3'
   get 'home-four' => 'home#experiment-4'
 
+  get 'info' => 'info#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
